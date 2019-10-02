84c426b6621d31a6a42e90ebd993ef76e775b299
PitchT <pitch_t@kkumail.com>
2019-10-02 17:17:52 +0700

    Secend na

--- src/TDD/TestCaseFizzbuzz.java
+++ src/TDD/TestCaseFizzbuzz.java
@@ -7,10 +7,15 @@ import org.junit.jupiter.api.Test;
 class TestCaseFizzbuzz {
 	FizzBuzz fizzBuzz = new FizzBuzz();
 	int i = fizzBuzz.getint(1);
+	int i2 = fizzBuzz.getint(2);
 	@Test
 	void input_is_1_expect_output_1() {
-		int input = 0;
 		assertEquals(1,i);
+	}
+	
+	@Test //2
+	void input_is_2_expect_output_2() {
+		assertEquals(2,i2);
 	}
 
 }
