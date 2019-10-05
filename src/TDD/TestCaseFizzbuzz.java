3ba3416b0c1f79d0dc387ae1caf86e567e9881cb
PitchT <pitch_t@kkumail.com>
2019-10-05 10:15:13 +0700

    Second

--- src/TDD/TestCaseFizzbuzz.java
+++ src/TDD/TestCaseFizzbuzz.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 class TestCaseFizzbuzz {
 	FizzBuzz fizzbuzz = new FizzBuzz();
 	String input1 = fizzbuzz.getnumber1(1);
+	String input2 = fizzbuzz.getnumber1(2);
 	String wordF ="Fizz";
 	String wordB ="Buzz";
 	String wordFB = "FizzBuzz";
@@ -14,6 +15,11 @@ class TestCaseFizzbuzz {
 	@Test
 	void test1() {
 		assertEquals("1", input1);
+	}
+	
+	@Test
+	void test2() {
+		assertEquals("2", input1);
 	}
 	
 }
